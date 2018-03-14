d8399e17023e81ea599b855c95d1f33237fd6904
Vidar Tonaas Fauske <vidartf@gmail.com>
2018-03-14 13:42:11 +0100

    Refactor to new name

--- setup.py
+++ setup.py
@@ -10,14 +10,12 @@ with io.open(os.path.join(cwd, 'README.rst'), encoding='utf-8') as fd:
 
 
 setup(
-    name='pytest-tornado',
-    version='0.4.5',
+    name='pytest-tornado5',
+    version='1.0.0',
     description=('A py.test plugin providing fixtures and markers '
                  'to simplify testing of asynchronous tornado applications.'),
     long_description=long_description,
-    url='https://github.com/eugeniy/pytest-tornado',
-    author='Eugeniy Kalinin',
-    author_email='burump@gmail.com',
+    url='https://github.com/vidartf/pytest-tornado',
     license='Apache License, Version 2.0',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -34,8 +32,8 @@ setup(
     keywords=('pytest py.test tornado async asynchronous '
               'testing unit tests plugin'),
     packages=find_packages(),
-    install_requires=['pytest', 'tornado>=4'],
+    install_requires=['pytest', 'tornado>=5'],
     entry_points={
-        'pytest11': ['tornado = pytest_tornado.plugin'],
+        'pytest11': ['tornado = pytest_tornado5.plugin'],
     },
 )
